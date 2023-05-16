3ce74a272491f048c131517565fb8dfa4d83d95b
Shen <72716300+shensandoval@users.noreply.github.com>
2023-05-16 15:49:52 +0800

    Create dbbConfig.js

---
+++ dbbConfig.js
@@ -0,0 +1,27 @@
+const mysql = require("mysql");
+const dotenv = require('dotenv')
+dotenv.config();
+
+const mltvlogs = require('../logger/logs')
+
+const mysqlConnection = mysql.createPool({
+    host: "18.1.0.123",
+    user: "sample_user",
+    password: "sjkdakdhuyqawinas",
+    database: "Sample_Database",
+    multipleStatements: true
+});
+
+mysqlConnection.getConnection((err) => {
+    if (!err) {
+        console.log("Connected kpusers.")
+        mltvlogs.loggerInfo.addContext('context', `--------------`);
+        mltvlogs.loggerInfo.info('Connection established.');
+    } else {
+        console.log("Connection Failed.", err)
+        mltvlogs.loggerError.addContext('context', `--------------`);
+        mltvlogs.loggerError.error('Error connecting to db.');
+    }
+})
+
+module.exports = mysqlConnection;
